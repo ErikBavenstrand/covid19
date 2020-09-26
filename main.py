62c01b1969b5cbd594198ad2e20197810e3e40ca
ErikBavenstrand <erik.bavenstrands@gmail.com>
2020-09-26 16:55:49 +0200

    Added wandb

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+# prevent tf debug logging
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 import tensorflow as tf
 
-tf.get_logger().setLevel('ERROR')
+import wandb
+from wandb import magic
+
+wandb.init(magic=True)
 
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255
+x_validation, y_validation = x_train[55000:], y_train[55000:]
+x_train, y_train = x_train[:55000], y_train[:55000]
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
@@ -14,16 +23,13 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(10)
 ])
 
-predictions = model(x_train[:1]).numpy()
-
-tf.nn.softmax(predictions).numpy()
-
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-
-loss_fn(y_train[:1], predictions).numpy()
 
 model.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=5)
+model.fit(x=x_train,
+          y=y_train,
+          validation_data=(x_validation, y_validation),
+          epochs=5)
 
-model.evaluate(x_test, y_test, verbose=2)
+model.evaluate(x=x_test, y=y_test, verbose=2)
