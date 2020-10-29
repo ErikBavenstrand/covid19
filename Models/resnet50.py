c998e9cd87d770fd080f2450406c875130bbe17f
ErikBavenstrand <erik.bavenstrands@gmail.com>
2020-10-29 13:35:46 +0100

    experimentationting

---
+++ Models/resnet50.py
@@ -0,0 +1,19 @@
+import numpy as np
+from tensorflow.keras.applications.resnet_v2 import ResNet50V2
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
+from tensorflow.keras import Model
+
+
+def model(img_width=224, img_height=224):
+    model_resnet50_conv = ResNet50V2(
+        include_top=False, input_shape=(img_width, img_height, 3)
+    )
+    for layer in model_resnet50_conv.layers[:86]:
+        layer.trainable = False
+
+    x = GlobalAveragePooling2D(name="GAP")(model_resnet50_conv.output)
+    x = Dense(2, activation="sigmoid", name="predictions")(x)
+
+    model = Model(inputs=model_resnet50_conv.inputs, outputs=x)
+
+    return model
